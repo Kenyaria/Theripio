6c1326393e8eb67b867f4e655ea1e467b4fe2e0a
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2026-04-06 11:01:09 -0400

    image banner
    added image banner with text

---
+++ src/components/BannerText.tsx
@@ -0,0 +1,10 @@
+const BannerText = () => {
+    return (
+        <div className="absolute top-2/5 left-1/2 bg-white/40 p-8 mr-5">
+            <h1 className="font-montserrat text-white text-sm md:text-xl lg:text-4xl font-bold">
+                Discovering tomorrow's<br></br>cures today.
+            </h1>
+        </div>
+    )
+}
+export default BannerText;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,18 @@
 import Navbar from "../components/Navbar";
+import Banner from "../components/Banner";
+import BannerText from "../components/BannerText";
+import Microscope from "../assets/banners/microscope2.jpg";
 
 const Home = () => {
     return (
         <div>
             <Navbar />
+            <Banner 
+                src={Microscope}
+                alt="Close up of a microscope."
+                height="h-[80dvh]"
+            />
+            <BannerText/>
             <h1>Home page</h1>
         </div>
     )
